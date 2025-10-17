84926a8e1ad1d668fc1616b9b3c574a6f3d3fbf6
ilucasbrandao <dev.lucasbrandao@gmail.com>
2025-10-17 16:18:06 -0300

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API rodando 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
